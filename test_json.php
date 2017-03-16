fd35c9401df3d7359727a61cc571dae8a6e98042
raowr <891770552@qq.com>
2017-03-16 15:48:19 +0800

    Signed-off-by: raowr <891770552@qq.com>

--- test_json.php
+++ test_json.php
@@ -9,22 +9,31 @@
 // echo json_encode($cars);
 // //var_dump($cars);
 // //print_r($cars);
-
-header('Content-Type:application/vnd.ms-excel');
-header('Content-Disposition:attachment;filename=demo.xls');
-header('Pragma:no-cache');
-header('Expires:0');
-$title = array('编号','姓名','性别','年龄','身高','体重');
-$data=array(
-		aray(1,'张山','男')
-		aray(1,'张山','男')
-		aray(1,'张山','男')
-		aray(1,'张山','男')
-		aray(1,'张山','男')
-);
-echo iconv('utf-8','gbk',implode("\t",$title)),"\n";
-foreach ($data as $value) {
-	# code...
-	echo iconv('utf-8','gbk',implode("\t",$value)),"\n";
+/**
+* 
+*/
+class test_excel
+{
+	
+	function f_excel()
+	{
+		# code...
+		header('Content-Type:application/vnd.ms-excel');
+		header('Content-Disposition:attachment;filename=demo.xls');
+		header('Pragma:no-cache');
+		header('Expires:0');
+		$title = array('编号','姓名','性别','年龄','身高','体重');
+		$data=array(
+			array(1,'张山','男',"22",183,72),
+			array(2,'李四','男',"18",170,50),
+			array(3,'王五','男',"14",178,68),
+			array(4,'赵六','男',"34",163,48)
+		);
+		echo iconv('utf-8','gbk',implode("\t",$title)),"\n";
+		foreach ($data as $value) {
+			echo iconv('utf-8','gbk',implode("\t",$value)),"\n";
+		}
+	}
 }
-
+$out_excel = new test_excel();
+$out_excel->f_excel();
