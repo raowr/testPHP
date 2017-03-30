a564d1fbf997d1c565bd2d3731c37743d8dfb841
raowr <891770552@qq.com>
2017-03-30 09:23:50 +0800

    Signed-off-by: raowr <891770552@qq.com>

--- test_php.php
+++ test_php.php
@@ -5,5 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+$mid=array();
+$mid['member_id']=2;
+$sql = "SELECT * FROM `vmc_commission_orderlog_achieve` WHERE `achieve_fund` > 0 AND `member_id` = $mid[member_id] ";
+		echo $sql;
